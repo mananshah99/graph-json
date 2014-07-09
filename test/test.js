7b0acecb5fb22447c5706b4871e2b3feefa8e205
Manan Shah <manan.shah.777@gmail.com>
2014-07-08 23:50:58 -0700

    Add mocha tests

---
+++ test/test.js
@@ -0,0 +1,44 @@
+var assert = require('assert');
+var graph = require('../lib/graph.js');
+var ts = require('./test_scheme.json');
+
+describe('Graph Creation', function () {
+    var graph_ = null;
+    before(function () {
+        graph_ = new graph(ts);
+    });
+
+    it('Edges and Nodes Exist', function () {
+        assert(graph_);
+        assert(graph_.nodes);
+        assert(graph_.edges);
+    });
+
+    it('Find Nodes and Edges by Name', function () {
+        var node = graph_.getNode('C');
+        assert(typeof node === "object");
+        assert(node.name === "C");
+        assert(node.data === "__DATA__");
+        
+        var edge = graph_.getEdge('BC');
+        
+        assert(typeof edge === "object");
+        assert(edge.from === "B");
+        assert(edge.to === "C");
+        assert(edge.name === "BC");
+    });
+
+    it('Get edges leaving from node', function () {
+        var x = graph_.edgesOut('A');
+        assert(x[0].to === 'B');
+    });
+
+    it('Get edges entering node', function () {
+        var x = graph_.edgesIn('B');
+        assert(x[0].from === 'A');
+    });
+
+    it('Determine terminal nodes', function () {
+        assert(graph_.isTerminal('T'));
+    });
+});
